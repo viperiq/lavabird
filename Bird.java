a654b1db846dc38b90819d30e47f26621f89d87b
MUSTAFA MAHMOUD KAREEM <104654188+viperiq@users.noreply.github.com>
2023-07-07 21:22:21 +0300

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,61 @@
+
+package javaapplication24;
+
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+
+public class Bird {
+    public float x, y, vx, vy;
+    final int PANEL_WIDTH = 800;
+	final int PANEL_HEIGHT = 600;
+    public static final int RAD = 25;
+    private Image img ,lava,BackGroundImage;
+    public Bird() {
+        x = FlappyBird.WIDTH/2;
+        y = FlappyBird.HEIGHT/2;
+        try {
+            img = ImageIO.read(new File("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\MyProject\\src\\images\\Bird.png"));
+            ImageIcon bg = new ImageIcon("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\JavaApplication24\\src\\images\\bg.jpg");
+                                   Image SI1 = bg.getImage().getScaledInstance(PANEL_WIDTH, PANEL_HEIGHT,java.awt.Image.SCALE_SMOOTH);
+                                   bg=new ImageIcon(SI1);
+                                   BackGroundImage = bg.getImage();
+                                   ImageIcon lv = new ImageIcon("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\JavaApplication24\\src\\images\\lava2.png");
+                                   Image SI2 = lv.getImage().getScaledInstance(800, 100,java.awt.Image.SCALE_SMOOTH);
+                                   lv=new ImageIcon(SI2);
+                                   lava = lv.getImage();
+                                   
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void physics() {
+        x+=vx;
+        y+=vy;
+        vy+=0.5f;
+    }
+    public void update(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawImage(BackGroundImage, 0, 0, null);
+        g.drawImage(lava, 0, 500, null);
+        g.drawImage(img, Math.round(x-RAD),Math.round(y-RAD),2*RAD,2*RAD, null);
+        
+    }
+    public void jump() {
+        vy = -8;
+    }
+    
+    public void reset() {
+        x = 640/2;
+        y = 640/2;
+        vx = vy = 0;
+    }
+}
---
+++ FlappyBird.java
@@ -0,0 +1,121 @@
+package javaapplication24;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author User
+ */
+public class FlappyBird implements ActionListener, KeyListener {
+    
+    public static final int FPS = 75, WIDTH = 800, HEIGHT = 600;
+    
+    private Bird bird;
+    private JFrame frame;
+    private JPanel panel;
+    private ArrayList<Rectangle> rects;
+    private int time, scroll;
+    private Timer t;
+    
+    private boolean paused;
+    
+    public void go() {
+        frame = new JFrame("Lava Bird");
+        bird = new Bird();
+        rects = new ArrayList<Rectangle>();
+        panel = new GamePanel(this, bird, rects);
+        frame.add(panel);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.addKeyListener(this);
+        
+        paused = true;
+        
+        t = new Timer(1000/FPS, this);
+        t.start();
+    }
+    public static void main(String[] args) {
+        new FlappyBird().go();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        panel.repaint();
+        if(!paused) {
+            bird.physics();
+            if(scroll % 90 == 0) {
+                Rectangle r = new Rectangle(WIDTH, 0, GamePanel.PIPE_W, (int) ((Math.random()*HEIGHT)/6f + (0.28f)*HEIGHT));
+                int h2 = (int) ((Math.random()*HEIGHT)/6f + (0.3f)*HEIGHT);
+                Rectangle r2 = new Rectangle(WIDTH, HEIGHT - h2, GamePanel.PIPE_W, h2);
+                rects.add(r);
+                rects.add(r2);
+            }
+            ArrayList<Rectangle> toRemove = new ArrayList<Rectangle>();
+            boolean game = true;
+            for(Rectangle r : rects) {
+                r.x-=3;
+                if(r.contains(bird.x, bird.y)) {game = false;
+                    toRemove.add(r);
+                }
+                if(r.contains(bird.x, bird.y)) {
+                    JOptionPane.showMessageDialog(frame, "You lose!\n"+"Your score was: "+time+".");
+                    game = false;
+                }
+                
+            }
+            rects.removeAll(toRemove);
+            time++;
+            scroll++;
+
+            if(bird.y > 500 || bird.y+bird.RAD < 0) {
+                game = false;
+            }
+
+            if(!game) {
+                rects.clear();
+                bird.reset();
+                time = 0;
+                scroll = 0;
+                paused = true;
+            }
+        }
+        
+    }
+    
+    public int getScore() {
+        return time;
+    }
+    
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_SPACE) {
+            bird.jump();
+        }
+        
+    }
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_SPACE) {
+            paused = false;
+        }
+    }
+    public void keyTyped(KeyEvent e) {
+        
+    }
+    
+    public boolean paused() {
+        return paused;
+    }
+}
