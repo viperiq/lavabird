bb9106b3ee570bcb9e49e51787e95e4a129361b8
MUSTAFA MAHMOUD KAREEM <104654188+viperiq@users.noreply.github.com>
2023-07-07 22:02:46 +0300

    Update Bird.java

--- LavaBird/Bird.java
+++ LavaBird/Bird.java
@@ -1,7 +1,4 @@
-
 package LavaBird;
-
-
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -11,44 +8,51 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
-
 public class Bird {
     public float x, y, vx, vy;
     final int PANEL_WIDTH = 800;
 	final int PANEL_HEIGHT = 600;
     public static final int RAD = 25;
     private Image img ,lava,BackGroundImage;
+    
     public Bird() {
         x = FlappyBird.WIDTH/2;
         y = FlappyBird.HEIGHT/2;
+        
+        // Load bird image
         try {
             img = ImageIO.read(new File("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\MyProject\\src\\images\\Bird.png"));
+            
+            // Load background image
             ImageIcon bg = new ImageIcon("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\JavaApplication24\\src\\images\\bg.jpg");
-                                   Image SI1 = bg.getImage().getScaledInstance(PANEL_WIDTH, PANEL_HEIGHT,java.awt.Image.SCALE_SMOOTH);
-                                   bg=new ImageIcon(SI1);
-                                   BackGroundImage = bg.getImage();
-                                   ImageIcon lv = new ImageIcon("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\JavaApplication24\\src\\images\\lava2.png");
-                                   Image SI2 = lv.getImage().getScaledInstance(800, 100,java.awt.Image.SCALE_SMOOTH);
-                                   lv=new ImageIcon(SI2);
-                                   lava = lv.getImage();
-                                   
-        }
-        catch(IOException e) {
+            Image SI1 = bg.getImage().getScaledInstance(PANEL_WIDTH, PANEL_HEIGHT, java.awt.Image.SCALE_SMOOTH);
+            bg = new ImageIcon(SI1);
+            BackGroundImage = bg.getImage();
+            
+            // Load lava image
+            ImageIcon lv = new ImageIcon("C:\\Users\\Lenovo\\Documents\\NetBeansProjects\\JavaApplication24\\src\\images\\lava2.png");
+            Image SI2 = lv.getImage().getScaledInstance(800, 100, java.awt.Image.SCALE_SMOOTH);
+            lv = new ImageIcon(SI2);
+            lava = lv.getImage();
+            
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
+    
     public void physics() {
-        x+=vx;
-        y+=vy;
-        vy+=0.5f;
+        x += vx;
+        y += vy;
+        vy += 0.5f;
     }
+    
     public void update(Graphics g) {
         g.setColor(Color.BLACK);
         g.drawImage(BackGroundImage, 0, 0, null);
         g.drawImage(lava, 0, 500, null);
-        g.drawImage(img, Math.round(x-RAD),Math.round(y-RAD),2*RAD,2*RAD, null);
-        
+        g.drawImage(img, Math.round(x - RAD), Math.round(y - RAD), 2 * RAD, 2 * RAD, null);
     }
+    
     public void jump() {
         vy = -8;
     }
